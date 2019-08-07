c8988d071394c6ac0df82a22b4ebb3e4ad55ae0a
jack96489 <giacomo.orsenigo@gmail.com>
2019-08-07 19:18:59 +0200

    Fix movimenti pacman

--- src/main/java/pacman/entity/Pacman.java
+++ src/main/java/pacman/entity/Pacman.java
@@ -4,10 +4,12 @@ import pacman.Direction;
 import pacman.PacmanGame;
 import pacman.mappa.Cella;
 
-import java.awt.*;
+import java.awt.Color;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Pacman extends BaseCreatura<Pacman> {
 
@@ -18,7 +20,7 @@ public class Pacman extends BaseCreatura<Pacman> {
     public Pacman() {
         super();
         color = Color.yellow;
-        nuoveDirezioni = new LinkedBlockingDeque<>(MAX_STORED_MOVES);
+        nuoveDirezioni = new LinkedBlockingQueue<>(MAX_STORED_MOVES);
     }
 
     @Override
@@ -47,44 +49,75 @@ public class Pacman extends BaseCreatura<Pacman> {
 
     @Override
     public void onTick() {
-        //System.out.println((x-X_BORDER)/CELL_WIDTH+"-"+(y-Y_BORDER)/CELL_HEIGHT);
-        Cella cellaAttuale = game.getGameMap().getCella(getTableX(), getTableY());
-        System.out.println("X: "+getTableX()+" Y: "+ getTableY());
+        checkMovement();
+        checkForMele();
+        checkForNewDir();
+    }
+
+    private void checkMovement() {
         int _x = x, _y = y;
         switch (currentDir) {
             case UP:
-                _y -= MOVEMENT_SPEED;
+                _y -= CELL_HEIGHT / 2 + MOVEMENT_ERROR_MARGIN/2;
                 break;
             case DOWN:
-                _y += MOVEMENT_SPEED;
+                _y += CELL_HEIGHT / 2 +MOVEMENT_ERROR_MARGIN/2;
                 break;
             case RIGHT:
-                _x += MOVEMENT_SPEED;
+                _x += CELL_WIDTH / 2+ MOVEMENT_ERROR_MARGIN/2;
                 break;
             case LEFT:
-                _x -= MOVEMENT_SPEED;
+                _x -= CELL_WIDTH / 2 + MOVEMENT_ERROR_MARGIN/2;
                 break;
         }
-        Cella nuova = game.getGameMap().getCella((_x - X_BORDER) / CELL_WIDTH, (_y - Y_BORDER) / CELL_HEIGHT);
+        System.out.println("X: " + x + "-" + _x);
+        System.out.println("Y: " + y + "-" + _y);
+        Cella nuova = game.getGameMap().getCella((_x + width / 2 - X_BORDER) / CELL_WIDTH, (_y + height / 2 - Y_BORDER) / CELL_HEIGHT);
 
         if (nuova.canBeUsed()) {
-            x = _x;
-            y = _y;
+            switch (currentDir) {
+                case UP:
+                    y -= MOVEMENT_SPEED;
+                    break;
+                case DOWN:
+                    y += MOVEMENT_SPEED;
+                    break;
+                case RIGHT:
+                    x += MOVEMENT_SPEED;
+                    break;
+                case LEFT:
+                    x -= MOVEMENT_SPEED;
+                    break;
+            }
         }
+    }
 
+    private void checkForNewDir() {
+        if (!nuoveDirezioni.isEmpty())
+            if (canChangeDir(nuoveDirezioni.peek()) && isCentered())
+                currentDir = nuoveDirezioni.poll();
+            else {
+                final List<Direction> newDirs = new ArrayList<>(nuoveDirezioni);
+                System.out.println(newDirs.size());
+                for (int i = 1; i < nuoveDirezioni.size(); i++)
+                    if (canChangeDir(newDirs.get(i)) && isCentered()) {
+                        currentDir = newDirs.get(i);
+                        nuoveDirezioni.clear();
+                        break;
+                    }
+            }
+    }
+
+    private void checkForMele() {
+        Cella cellaAttuale = game.getGameMap().getCella(getTableX(), getTableY());
         if (cellaAttuale.getTipoCella() == Cella.TipoCella.MELA) {
             PacmanGame.getInstance().melaMangiata();
             cellaAttuale.melaMangiata();
         }
-
-
-        for (Direction dir : nuoveDirezioni) {
-            if (canChangeDir(dir) && isCentered()) {
-                currentDir = dir;
-                nuoveDirezioni.clear();
-            }
+        if (cellaAttuale.getTipoCella() == Cella.TipoCella.MELONE) {
+            PacmanGame.getInstance().melonaMangiato();
+            cellaAttuale.melaMangiata();
         }
-
     }
 
     private boolean canChangeDir(Direction dir) {
@@ -95,14 +128,15 @@ public class Pacman extends BaseCreatura<Pacman> {
     }
 
     private boolean isCentered() {
-        System.out.println("x " + getMiddleX());
-        System.out.println("table x " + (getTableX()));
-        System.out.println("table " + (getTableX()) * CELL_WIDTH);
-        System.out.println("border " + X_BORDER);
-        System.out.println("relative x " + (getMiddleX() - X_BORDER - (getTableX()) * CELL_WIDTH));
-        System.out.println(getMiddleX() - X_BORDER - (getTableX()) * CELL_WIDTH - CELL_WIDTH / 2);
+//        System.out.println("x " + getMiddleX());
+//        System.out.println("table x " + (getTableX()));
+//        System.out.println("table " + (getTableX()) * CELL_WIDTH);
+//        System.out.println("border " + X_BORDER);
+//        System.out.println("relative x " + (getMiddleX() - X_BORDER - (getTableX()) * CELL_WIDTH));
+//        System.out.println(getMiddleX() - X_BORDER - (getTableX()) * CELL_WIDTH - CELL_WIDTH / 2);
         if (Math.abs(getMiddleX() - X_BORDER - (getTableX()) * CELL_WIDTH - CELL_WIDTH / 2) < MOVEMENT_ERROR_MARGIN && Math.abs(getMiddleY() - Y_BORDER - (getTableY()) * CELL_HEIGHT - CELL_HEIGHT / 2) < MOVEMENT_ERROR_MARGIN)
             return true;
+        System.out.println("Not centered");
         return false;
     }
 
