b5c4dc9dc5904efb21702c8cd17ad948d93ff561
jack96489 <giacomo.orsenigo@gmail.com>
2019-08-10 20:05:09 +0200

    teletrasporto

--- src/main/java/pacman/entity/BaseCreatura.java
+++ src/main/java/pacman/entity/BaseCreatura.java
@@ -18,7 +18,7 @@ public abstract class BaseCreatura<T extends BaseCreatura> extends BaseRenderabl
     private boolean appenaMorto = false;
 
     public BaseCreatura() {
-        this(X_BORDER + NUM_COLONNE/2*CELL_WIDTH+(CELL_WIDTH-CREATURA_WIDTH)/2, Y_BORDER +NUM_RIGHE/2*CELL_HEIGHT+(CELL_HEIGHT-CREATURA_HEIGHT)/2, CREATURA_WIDTH, CREATURA_HEIGHT, Color.BLACK);
+        this(X_BORDER + NUM_COLONNE / 2 * CELL_WIDTH + (CELL_WIDTH - CREATURA_WIDTH) / 2, Y_BORDER + NUM_RIGHE / 2 * CELL_HEIGHT + (CELL_HEIGHT - CREATURA_HEIGHT) / 2, CREATURA_WIDTH, CREATURA_HEIGHT, Color.BLACK);
     }
 
     public BaseCreatura(int x, int y, int width, int height, Color color) {
@@ -44,44 +44,55 @@ public abstract class BaseCreatura<T extends BaseCreatura> extends BaseRenderabl
     }
 
     protected boolean checkMovement() {
-        int _x = x, _y = y;
-        switch (currentDir) {
-            case UP:
-                _y -= CELL_HEIGHT / 2 + MOVEMENT_ERROR_MARGIN/2;
-                break;
-            case DOWN:
-                _y += CELL_HEIGHT / 2 +MOVEMENT_ERROR_MARGIN/2;
-                break;
-            case RIGHT:
-                _x += CELL_WIDTH / 2+ MOVEMENT_ERROR_MARGIN/2;
-                break;
-            case LEFT:
-                _x -= CELL_WIDTH / 2 + MOVEMENT_ERROR_MARGIN/2;
-                break;
-        }
-//        System.out.println("X: " + x + "-" + _x);
-//        System.out.println("Y: " + y + "-" + _y);
-        Cella nuova = game.getGameMap().getCella((_x + width / 2 - X_BORDER) / CELL_WIDTH, (_y + height / 2 - Y_BORDER) / CELL_HEIGHT);
-
-        if (nuova.canBeUsed()) {
+        System.out.println(TABLE_WIDTH + X_BORDER - (x + width));
+        if (x - X_BORDER < MOVEMENT_SPEED + MOVEMENT_ERROR_MARGIN && currentDir == Direction.LEFT) {
+            x += TABLE_WIDTH - width;
+            return true;
+        } else if (TABLE_WIDTH + X_BORDER - (x + width) < MOVEMENT_SPEED + MOVEMENT_ERROR_MARGIN && currentDir == Direction.RIGHT) {
+            x -= TABLE_WIDTH - width;
+            return true;
+        } else {
+            int _x = x, _y = y;
             switch (currentDir) {
                 case UP:
-                    y -= MOVEMENT_SPEED;
+                    _y -= CELL_HEIGHT / 2 + MOVEMENT_ERROR_MARGIN / 2;
                     break;
                 case DOWN:
-                    y += MOVEMENT_SPEED;
+                    _y += CELL_HEIGHT / 2 + MOVEMENT_ERROR_MARGIN / 2;
                     break;
                 case RIGHT:
-                    x += MOVEMENT_SPEED;
+                    _x += CELL_WIDTH / 2 + MOVEMENT_ERROR_MARGIN / 2;
                     break;
                 case LEFT:
-                    x -= MOVEMENT_SPEED;
+                    _x -= CELL_WIDTH / 2 + MOVEMENT_ERROR_MARGIN / 2;
                     break;
             }
-            return true;
+//        System.out.println("X: " + x + "-" + _x);
+//        System.out.println("Y: " + y + "-" + _y);
+            int tableX = (_x + width / 2 - X_BORDER) / CELL_WIDTH, tableY = (_y + height / 2 - Y_BORDER) / CELL_HEIGHT;
+            Cella nuova = game.getGameMap().getCella(tableX, tableY);
+
+            if (nuova.canBeUsed()) {
+                switch (currentDir) {
+                    case UP:
+                        y -= MOVEMENT_SPEED;
+                        break;
+                    case DOWN:
+                        y += MOVEMENT_SPEED;
+                        break;
+                    case RIGHT:
+                        x += MOVEMENT_SPEED;
+                        break;
+                    case LEFT:
+                        x -= MOVEMENT_SPEED;
+                        break;
+                }
+                return true;
+            }
         }
         return false;
     }
+
     protected boolean canChangeDir(Direction dir) {
         Cella adiacente = game.getGameMap().getCellaAdiacente(getTableX(), getTableY(), dir);
         if (adiacente.canBeUsed())
